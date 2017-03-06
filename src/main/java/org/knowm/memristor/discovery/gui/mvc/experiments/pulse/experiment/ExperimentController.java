dce1794fd1220f0bb8be9cce1ede2dd3111b5793
Tim Molter <tim@knowm.org>
2017-03-06 14:45:14 -0800

    added keystroke action for “S” to start and stop experiment

--- src/main/java/org/knowm/memristor/discovery/gui/mvc/experiments/pulse/experiment/ExperimentController.java
+++ src/main/java/org/knowm/memristor/discovery/gui/mvc/experiments/pulse/experiment/ExperimentController.java
@@ -34,10 +34,13 @@ import java.awt.event.KeyEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
+import javax.swing.AbstractAction;
 import javax.swing.AbstractButton;
 import javax.swing.BorderFactory;
+import javax.swing.JComponent;
 import javax.swing.JSlider;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
@@ -181,6 +184,16 @@ public class ExperimentController implements PropertyChangeListener {
         experimentModel.setMemristorVoltageDropSelected(selected);
       }
     });
+
+    experimentPanel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("S"), "fire");
+    experimentPanel.getActionMap().put("fire", new AbstractAction() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+
+        experimentPanel.getStartStopButton().doClick();
+      }
+    })
+    ;
   }
 
   /**
