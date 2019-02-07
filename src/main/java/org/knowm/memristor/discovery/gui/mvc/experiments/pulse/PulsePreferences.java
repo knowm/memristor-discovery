1de697a1b550cded8f7e7fc20aab8dddc08796fc
Tim Molter <tim.molter@gmail.com>
2019-02-07 09:33:52 +0100

    update licence date

--- src/main/java/org/knowm/memristor/discovery/gui/mvc/experiments/pulse/PulsePreferences.java
+++ src/main/java/org/knowm/memristor/discovery/gui/mvc/experiments/pulse/PulsePreferences.java
@@ -3,7 +3,7 @@
  * and is also available under alternative licenses negotiated directly
  * with Knowm, Inc.
  *
- * Copyright (c) 2016-2018 Knowm Inc. www.knowm.org
+ * Copyright (c) 2016-2019 Knowm Inc. www.knowm.org
  *
  * This package also includes various components that are not part of
  * Memristor-Discovery itself:
