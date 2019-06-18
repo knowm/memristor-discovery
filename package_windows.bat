e66bdddb69287c405628e7d1088dd6120b2e76a6
Tim Molter <tim.molter@gmail.com>
2019-06-18 15:45:01 +0200

    update to 2.0.0

--- package_windows.bat
+++ package_windows.bat
@@ -1,7 +1,7 @@
 set INPUT=jar
 set OUTPUT=_exe
-set JAR=memristor-discovery-0.0.9.jar
-set VERSION=0.0.9
+set JAR=memristor-discovery-2.0.0.jar
+set VERSION=2.0.0
 set APP_ICON=_exe/icons.ico
 
 call "%JAVA_HOME%\bin\java.exe" ^
